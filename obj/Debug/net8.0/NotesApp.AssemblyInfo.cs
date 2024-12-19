178c5246d362fdda841588fc110b47b753cfeade
Christopher <chrissjustice07@gmail.com>
2024-12-19 18:07:20 +0300

    First pull cimmit

--- obj/Debug/net8.0/NotesApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/NotesApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6047962b3a767d2f9504e2366b9dc5f0a1f818c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
